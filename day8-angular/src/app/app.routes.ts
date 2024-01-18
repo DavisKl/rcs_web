06aa611d57e19873954f756edaccd7746bd9df9f
DavisKl <150376554+DavisKl@users.noreply.github.com>
2024-01-18 21:34:10 +0200

    7

--- day8-angular/src/app/app.routes.ts
+++ day8-angular/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'main', component: MainComponent},
+    {path:'header', component: HeaderComponent},
+    {path:'footer', component: FooterComponent}
+];
