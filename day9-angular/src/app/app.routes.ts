efd6ff20a470bc0355f2357ceec56ab5e7b54d1b
DavisKl <150376554+DavisKl@users.noreply.github.com>
2024-01-23 20:46:20 +0200

    ds

--- day9-angular/src/app/app.routes.ts
+++ day9-angular/src/app/app.routes.ts
@@ -1,19 +1,12 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 import { AdditionComponent } from './addition/addition.component';
 import { SubstractionComponent } from './substraction/substraction.component';
 import { MultiplyComponent } from './multiply/multiply.component';
 import { DivideComponent } from './divide/divide.component';
 
 export const routes: Routes = [
-    {path:'header', component: HeaderComponent},
     {path:'addition', component: AdditionComponent},
     {path:'substraction', component: SubstractionComponent},
     {path:'multiply', component: MultiplyComponent},
     {path:'divide', component: DivideComponent},
-    {path: '', redirectTo: '/addition', pathMatch: 'full'},
-    {path: '', redirectTo: '/substraction', pathMatch: 'full'},
-    {path: '', redirectTo: '/multiply', pathMatch: 'full'},
-    {path: '', redirectTo: '/divide', pathMatch: 'full'},
-
 ];
